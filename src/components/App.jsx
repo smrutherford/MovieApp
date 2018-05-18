71f2b97be93836d90b34d7b6835db1d2b0ab3cdc
Stacey Rutherford <rutherford.stacey@gmail.com>
2018-05-17 22:29:38 -0700

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+// import React from "react"; // webpack functionality, but JS commands
+// import ReactDOM from "react-dom";
+// need to wrap each react component in element tag
+function App() {
+	return (
+		<div> 
+			<h2>MovieList</h2>
+			<div className="movies">
+				"hello world"
+			</div>
+		</div>
+	)
+}
+
+window.App = App;
